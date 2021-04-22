1fbed46e3a52be973785eb6f84c058b79fbed978
YihSong <yihsong12@gmail.com>
2021-04-22 14:42:44 +0800

    Init

---
+++ Assets/Scripts/Pitch/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody rb;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MoveRotation(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
