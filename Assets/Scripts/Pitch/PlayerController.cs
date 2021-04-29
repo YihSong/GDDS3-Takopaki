584353a32dd80abe7f7e89ad326b829b406013ff
YihSong <yihsong12@gmail.com>
2021-04-29 16:12:40 +0800

    Revert "dfzh"
    This reverts commit dd182a48864815ce3cb4cedadb3d717a9c264d80.

--- Assets/Scripts/Pitch/PlayerController.cs
+++ Assets/Scripts/Pitch/PlayerController.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    Vector3 movementDirection;
-    float lookDirection;
 
 
 
@@ -23,34 +20,18 @@ public class PlayerController : MonoBehaviour
         // ----------------------------------------------------------------------------------------
         // ----------------------------------------------------------------------------------------
 
-        //float xMov = Input.GetAxisRaw("Horizontal");
-        //float zMov = Input.GetAxisRaw("Vertical");
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
         
-        //movementDirection = new Vector3(xMov, 0, zMov).normalized * speed * Time.deltaTime;
-        //movementDirection.Normalize();
+        Vector3 movementDirection = new Vector3(xMov, 0, zMov).normalized * speed * Time.deltaTime;
+        movementDirection.Normalize();
 
-        //transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
 
         if (movementDirection != Vector3.zero)
         {
             transform.forward = movementDirection;
         }
-        // ----------------------------------------------------------------------------------------
-        //-----------------------------------------------------------------------------------------
-
-
-
-
-
-
-        // PLAYER ROTATION
-        // ----------------------------------------------------------------------------------------
-        // ----------------------------------------------------------------------------------------
-
-        //float heading = Mathf.Atan2(Input.GetAxisRaw("Horizontal Look"), Input.GetAxisRaw("Vertical Look"));
-        //if (Input.GetAxisRaw("Horizontal Look") + Input.GetAxisRaw("Vertical Look") != 0)
-        //{
-        //    transform.rotation = Quaternion.Euler(0f, heading * Mathf.Rad2Deg, 0f);
 
         // ----------------------------------------------------------------------------------------
         //-----------------------------------------------------------------------------------------
@@ -67,27 +48,4 @@ public class PlayerController : MonoBehaviour
         // ----------------------------------------------------------------------------------------
     }
 
-    {
-        //Player movement
-        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
-
-        //Player rotation
-        if (lookDirection != 0)
-        {
-            transform.rotation = Quaternion.Euler(0f, lookDirection * Mathf.Rad2Deg, 0f);
-        }
-    }
-
-    void OnMove(InputValue input)
-    {
-        Vector2 inputValue = input.Get<Vector2>();
-        movementDirection = new Vector3(inputValue.x, 0, inputValue.y);
-        movementDirection.Normalize();
-    }
-
-    void OnLook(InputValue input)
-    {
-        Vector2 inputValue = input.Get<Vector2>();
-        lookDirection = Mathf.Atan2(inputValue.x, inputValue.y);
-    }
 }
