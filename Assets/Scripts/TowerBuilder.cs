4f170139a56df32a5b4a7e5c3c7490f3f9107d32
Aryan Isran <aryanisran@gmail.com>
2021-06-09 19:11:14 +0800

    random towe spawning

--- Assets/Scripts/TowerBuilder.cs
+++ Assets/Scripts/TowerBuilder.cs
@@ -5,35 +5,39 @@ using UnityEngine;
 public class TowerBuilder : MonoBehaviour
 {
     public GameObject[] towerPrefabs;
+    public Transform[] tiles;
     public LayerMask layer;
     public float offset;
+    int j = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shuffle();
+    }
+
+    void Shuffle()
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            Transform temp = tiles[i];
+            int rand = Random.Range(0, tiles.Length);
+            tiles[i] = tiles[rand];
+            tiles[rand] = temp;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            BuildTower();
-        }
+
     }
 
     public void BuildTower()
     {
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if(Physics.Raycast(ray, out hit, Mathf.Infinity, layer))
-        {
-            if (hit.transform.gameObject.GetComponent<Tile>().occupied) return;
-            GameObject towerToBuild = towerPrefabs[Random.Range(0, towerPrefabs.Length)];
-            Instantiate(towerToBuild, hit.transform.position + Vector3.up * offset, Quaternion.identity);
-            hit.transform.gameObject.GetComponent<Tile>().occupied = true;
-        }
+        if (j > tiles.Length) return;
+        GameObject towerToBuild = towerPrefabs[Random.Range(0, towerPrefabs.Length)];
+        Instantiate(towerToBuild, tiles[j].position + Vector3.up * offset, Quaternion.identity);
+        j++;
     }
 }
