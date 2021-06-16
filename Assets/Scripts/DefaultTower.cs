02bdf340b5015c5314db6e253ce1d14b66ffacd0
YihSong <64509890+YihSong@users.noreply.github.com>
2021-06-16 13:16:33 +0800

    enemy track

--- Assets/Scripts/DefaultTower.cs
+++ Assets/Scripts/DefaultTower.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class DefaultTower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    public float range;
+
+    public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+
+
+    float turnSpeed = 10f;
+
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
-    // Update is called once per frame
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        // Found enemy in our range
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+        // Tracking the enemy 
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
     }
 }
