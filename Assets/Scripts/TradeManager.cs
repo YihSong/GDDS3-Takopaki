a36007f7fa6e1444d1ea8046be2248f9d2636e69
YihSong <64509890+YihSong@users.noreply.github.com>
2021-06-20 17:06:56 +0800

    tryry

--- Assets/Scripts/TradeManager.cs
+++ Assets/Scripts/TradeManager.cs
@@ -7,9 +7,14 @@ public class TradeManager : MonoBehaviour
 {
     public GameObject player1Confirmation;
     public TowerBuilder td;
+
+    public GameObject tradeButton;
+
+    public TradeConfirmation confirmation;
     private void Start()
     {
         td = FindObjectOfType<TowerBuilder>();
+        confirmation = FindObjectOfType<TradeConfirmation>();
     }
     // Update is called once per frame
     void Update()
@@ -21,6 +26,15 @@ public class TradeManager : MonoBehaviour
         else
         {
             player1Confirmation.SetActive(false);
+        }
+
+        if (confirmation.mySideConfirm == true && confirmation.otherSideConfirm == true)
+        {
+            tradeButton.SetActive(true);
+        }
+        else
+        {
+            tradeButton.SetActive(false);
         }
     }
 }
